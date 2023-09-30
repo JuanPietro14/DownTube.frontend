421bfde0db655b6ef8db953c1d3a5a30876d5e21
Daniel Rodrigues <rodriguex.daniel89@gmail.com>
2023-09-30 20:33:50 -0300

    criacao do metodo receiveurl

--- webapp/app/Http/Controllers/DowntubeController.php
+++ webapp/app/Http/Controllers/DowntubeController.php
@@ -9,4 +9,7 @@ class DowntubeController extends Controller
     public function index(){
         return view('downtube-pt');
     }
+
+    public function receiveUrl(){}
+    
 }
--- webapp/resources/views/downtube-pt.blade.php
+++ webapp/resources/views/downtube-pt.blade.php
@@ -26,14 +26,18 @@
             <p class="dica">Bem vindo ao Downtube! Informe a URL do vídeo que deseja baixar: </p>
         </div>
         
-        <div class="container">
+        <form method="POST" action="" class="container">
+            @csrf
+        
+
+
             <input type="text" class="url" placeholder="Informe a URL do vídeo para efetuar o download"> <span> 
                 <button class="download"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
                     <polyline points="7 10 12 15 17 10"/>
                     <line x1="12" y1="15" x2="12" y2="3"/>
                 </svg> Download</button></span>
-        </div>
+        </form>
 
         <hr>
         <div class="scope">
